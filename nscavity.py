2b02cd0b583db6462d8967158dd79808f7fedd0e
Gil <gilforsyth@gmail.com>
2013-02-17 18:53:07 -0500

    plots not working on nscavity flow

--- nscavity.py
+++ nscavity.py
@@ -1,21 +1,25 @@
 ##Cavity Flow Navier Stokes
-#from mpl_toolkits.mplot3d import Axes3D
-#from matplotlib import cm
-#from matplotlib.ticker import LinearLocator, FormatStrFormatter
-#import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
 import numpy as np
-#import os
+import os
 
-#plt.ion()
+plt.ion()
 
 ##variable declarations
 nx = 31
 ny = 31
-nt = 100
+nt =10  
 c = 1
 dx = 2.0/(nx-1)
 dy = 2.0/(ny-1)
 dt = dx*dy
+x = np.linspace(0,2,nx)
+y = np.linspace(0,2,ny)
+
+X,Y = np.meshgrid(x, y)
 
 ##physical variables
 rho = 1
@@ -52,7 +56,26 @@ for n in range(nt):
 	p[1:-1,1:-1] = ((pn[2:,1:-1]+pn[0:-2,1:-1])*dy**2+(pn[1:-1,2:]+pn[1:-1,0:-2])*dx**2)/\
 			(2*(dx**2+dy**2)) -\
 			rho*dx**2*dy**2/(2*(dx**2+dy**2))*\
-			((1/dt*((un[2:,1:-2]-un[0:-2,1:-1])/(2*dx)+(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy)))-\
-			(un[2:,1:-1]-un[0:-2,1:-1])/(2*dx)*(un[2:,1:-1]-un[0:-2,1:-1])/(2*dx)-\	
+			((1/dt*((un[2:,0:-2]-un[0:-2,1:-1])/(2*dx)+(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy)))-\
+			(un[2:,1:-1]-un[0:-2,1:-1])/(2*dx)*(un[2:,1:-1]-un[0:-2,1:-1])/(2*dx)-\
 			2*(un[1:-1,2:]-un[1:-1,0:-2])/(2*dy)*(vn[2:,1:-1]-vn[0:-2,1:-1])/(2*dx)-\
-			(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy)*(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy)-\	
+			(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy)*(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy))
+	plt.streamplot(X,Y,u,v)
+###
+###7	.	.	.	.	.
+###6	.	.	.	.	.
+###5	.	.	.	.	.
+###4	.	.	.	.	.
+###3	.	.	.	.	.
+###2	.	.	.	.	.
+###1	.	.	.	.	.
+###0	.	.	.	.	.
+###     0	1	2	3	4
+
+	u[:],v[:] = 0,0		##u, v = 0 everywhere except
+	u[-1,:] = 1		## at y = 2 where u = 1
+	p[-1,:] = 0		##p = 0 at y = 2
+	p[0,:] = p[1,:]		##dp/dy = 0 at y = 0
+	p[:,0]=p[:,1]		##dp/dx = 0 at x = 0
+	p[:,-1]=p[:,-2]		##dp/dx = 0 at x = 2
+	plt.show()

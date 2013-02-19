bdae19ab3af76b946bd4a080b701e40f0bc4c4ea
Gil <gilforsyth@gmail.com>
2013-02-19 15:19:01 -0500

    need to space out plots, BCs are fixed

--- nscavity.py
+++ nscavity.py
@@ -9,22 +9,22 @@ import os
 plt.ion()
 
 ##variable declarations
-nx = 31
-ny = 31
-nt =10  
+nx = 41
+ny = 41
+nt =100  
 c = 1
 dx = 2.0/(nx-1)
 dy = 2.0/(ny-1)
-dt = dx*dy
 x = np.linspace(0,2,nx)
 y = np.linspace(0,2,ny)
 
-X,Y = np.meshgrid(x, y)
+Y,X = np.meshgrid(y,x)
 
 ##physical variables
-rho = 1
-nu = 1
+rho = .2
+nu = .2
 
+dt = dx*dy*nu*rho
 ##initial conditions
 u = np.zeros((ny,nx)) ##create a XxY vector of 0's
 un = np.zeros((ny,nx)) ##create a XxY vector of 0's
@@ -36,6 +36,13 @@ p = np.zeros((ny,nx)) ##create a XxY vector of 0's
 pn = np.zeros((ny,nx)) ##create a XxY vector of 0's
 
 
+#Y,X = np.mgrid[0:2:31j,0:2:31j]
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_wireframe(X,Y,p[:])
+plt.draw()
+
 for n in range(nt):
 	un[:] = u[:]
 	vn[:] = v[:]
@@ -44,23 +51,22 @@ for n in range(nt):
 	u[1:-1,1:-1] = un[1:-1,1:-1]-un[1:-1,1:-1]*dt/dx*(un[1:-1,1:-1]-un[0:-2,1:-1])-\
 		vn[1:-1,1:-1]*dt/dx*(un[1:-1,1:-1]-un[1:-1,0:-2])-\
 		dt/(2*rho*dx)*(pn[2:,1:-1]-pn[0:-2,1:-1])+\
-		nu*(dt/dx**2*(un[2:,1:-1]-2*un[1:-1,1:-1]+un[0:-2,1:-1]))+\
-		nu*(dt/dy**2*(un[1:-1,2:]-2*un[1:-1,1:-1]+un[1:-1,0:-2]))
+		nu*(dt/dx**2*(un[2:,1:-1]-2*un[1:-1,1:-1]+un[0:-2,1:-1])+\
+		dt/dy**2*(un[1:-1,2:]-2*un[1:-1,1:-1]+un[1:-1,0:-2]))
 	
 	v[1:-1,1:-1] = vn[1:-1,1:-1]-un[1:-1,1:-1]*dt/dx*(vn[1:-1,1:-1]-vn[0:-2,1:-1])-\
 		vn[1:-1,1:-1]*dt/dx*(vn[1:-1,1:-1]-vn[1:-1,0:-2])-\
-		dt/(2*rho*dx)*(pn[2:,1:-1]-pn[0:-2,1:-1])+\
-		nu*(dt/dx**2*(vn[2:,1:-1]-2*vn[1:-1,1:-1]+vn[0:-2,1:-1]))+\
-		nu*(dt/dy**2*(vn[1:-1,2:]-2*vn[1:-1,1:-1]+vn[1:-1,0:-2]))
+		dt/(2*rho*dy)*(pn[1:-1,2:]-pn[1:-1,0:-2])+\
+		nu*(dt/dx**2*(vn[2:,1:-1]-2*vn[1:-1,1:-1]+vn[0:-2,1:-1])+\
+		(dt/dy**2*(vn[1:-1,2:]-2*vn[1:-1,1:-1]+vn[1:-1,0:-2])))
 
 	p[1:-1,1:-1] = ((pn[2:,1:-1]+pn[0:-2,1:-1])*dy**2+(pn[1:-1,2:]+pn[1:-1,0:-2])*dx**2)/\
-			(2*(dx**2+dy**2)) -\
-			rho*dx**2*dy**2/(2*(dx**2+dy**2))*\
-			((1/dt*((un[2:,0:-2]-un[0:-2,1:-1])/(2*dx)+(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy)))-\
-			(un[2:,1:-1]-un[0:-2,1:-1])/(2*dx)*(un[2:,1:-1]-un[0:-2,1:-1])/(2*dx)-\
-			2*(un[1:-1,2:]-un[1:-1,0:-2])/(2*dy)*(vn[2:,1:-1]-vn[0:-2,1:-1])/(2*dx)-\
-			(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy)*(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy))
-	plt.streamplot(X,Y,u,v)
+		(2*(dx**2+dy**2)) -\
+		rho*dx**2*dy**2/(2*(dx**2+dy**2))*\
+		((1/dt*((un[2:,1:-1]-un[0:-2,1:-1])/(2*dx)+(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy)))-\
+		(un[2:,1:-1]-un[0:-2,1:-1])/(2*dx)*(un[2:,1:-1]-un[0:-2,1:-1])/(2*dx)-\
+		2*(un[1:-1,2:]-un[1:-1,0:-2])/(2*dy)*(vn[2:,1:-1]-vn[0:-2,1:-1])/(2*dx)-\
+		(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy)*(vn[1:-1,2:]-vn[1:-1,0:-2])/(2*dy))
 ###
 ###7	.	.	.	.	.
 ###6	.	.	.	.	.
@@ -72,10 +78,24 @@ for n in range(nt):
 ###0	.	.	.	.	.
 ###     0	1	2	3	4
 
-	u[:],v[:] = 0,0		##u, v = 0 everywhere except
+#	u[0,:] = 0
+#	u[:,0] = 0
+#	u[:,-1] = 0
+#	v[0,:] = 0
+#	v[-1,:]=0
+#	v[:,0] = 0
+#	v[:,-1] = 0
+	
+	u[:]=0
+	v[:]=0
 	u[-1,:] = 1		## at y = 2 where u = 1
 	p[-1,:] = 0		##p = 0 at y = 2
 	p[0,:] = p[1,:]		##dp/dy = 0 at y = 0
 	p[:,0]=p[:,1]		##dp/dx = 0 at x = 0
 	p[:,-1]=p[:,-2]		##dp/dx = 0 at x = 2
-	plt.show()
+#	plt.streamplot(X,Y,u,v)
+#	plt.show()
+	print p
+	surf = ax.plot_wireframe(X,Y,p[:])
+	plt.draw()
+	wait = raw_input('enter to continue')

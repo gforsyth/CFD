aac9dd393ef09a8e58aa1c538a6ef9bbcc0ae393
Gilbert Forsyth <gforsyth@hpcl-25.bu.edu>
2013-04-25 14:47:00 -0400

    working cuda 1d nonlinear

--- 1d_nonlinear_numpy.py
+++ 1d_nonlinear_numpy.py
@@ -1,37 +1,95 @@
 ##1D linear convection
-from pylab import *
+import matplotlib.pyplot as plt
+import numpy
 import time
-
-###Enter python interactive mode
-ion()
+from numbapro import autojit, cuda, jit, float32
 
 
-###Variable declarations
-nx = 81
-nt = 100
-c = 1
-xmax = 4.0
-dx = xmax/(nx-1)
-sigma = 0.25
-dt = sigma*dx
-
-###Assign initial conditions
-u = ones((1,nx)) ##create a 1xn vector of 1's
-u[0,.5/dx:1/dx+1]=2 ##set hat function I.C. : .5<=x<=1 is 2
-un = ones((1,nx))    
 
 
-###Plot first frame of animation (ICs)
-line, = plot(linspace(0,xmax,nx),u[0,:],marker='o',lw=2)
+def NonLinNumpy(u, un, nx, nt, dx, dt):
+
+    ###Run through nt timesteps and plot/animate each step
+    for n in range(nt): ##loop across number of time steps
+        un[:] = u[:]
+        u[1:] = -un[1:]*dt/dx*(un[1:]-un[:-1])+un[1:]
+    
+    return u
+
+def NonLinVanilla(u, nx, nt, dx, dt):
+
+    for n in range(nt):
+        for i in range(1,nx-1):
+            u[i+1] = -u[i]*dt/dx*(u[i]-u[i-1])+u[i]
+
+    return u
+
+#@vectorize([float32[:], float32, float32, float32, float32, float32, float32], target='cpu')
+@autojit
+def NonLinNumba(u,un, nx, nt, dx, dt):
+
+    for n in range(nt):
+        for i in range(1,nx):
+            un[i] = -u[i]*dt/dx*(u[i]-u[i-1])+u[i]
+
+    return un
+
+@jit(argtypes=[float32[:], float32, float32, float32[:]], target='gpu')
+def NonLinCudaJit(u, dx, dt, un):
+    tid = cuda.threadIdx.x
+    blkid = cuda.blockIdx.x
+    blkdim = cuda.blockDim.x
+    i = tid + blkid * blkdim
+    un[i] = -u[i]*dt/dx*(u[i]-u[i-1])+u[i]
 
 
-###Run through nt timesteps and plot/animate each step
-for n in range(nt): ##loop across number of time steps
-	un[:] = u[:]
-	u[0,1:] = -un[0,1:]*dt/dx*(un[0,1:]-un[0,:-1])+un[0,1:]
-	line.set_ydata(u[0,:])
-	draw()
-	title('1D Nonlinear Convection')  
-  
+def main():
+    ##System Conditions    
+    nx = 2457600
+    nt = 300
+    c = 1
+    xmax = 15.0
+    dx = xmax/(nx-1)
+    sigma = 0.25
+    dt = sigma*dx
+
+    ##Initial Conditions for wave
+    ui = numpy.ones(nx) ##create a 1xn vector of 1's
+    ui[.5/dx:1/dx+1]=2 ##set hat function I.C. : .5<=x<=1 is 2
+    un = numpy.ones(nx)    
+
+    t1 = time.time()
+    u = NonLinNumpy(ui, un, nx, nt, dx, dt)
+    t2 = time.time()
+    print "Numpy version took: %.6f seconds" % (t2-t1)
+    plt.plot(numpy.linspace(0,xmax,nx),u[:],marker='o',lw=2)
+   
+    t1 = time.time()
+    u = NonLinNumba(ui, un, nx, nt, dx, dt)
+    t2 = time.time()
+    print "Numbapro Vectorize version took: %.6f seconds" % (t2-t1)
+    plt.plot(numpy.linspace(0,xmax,nx),u[:],marker='o',lw=2)
+
+    griddim = 10, 10
+    blockdim = 768,32,1 
+    NonLinCudaJit_conf = NonLinCudaJit[griddim, blockdim]
+    t1 = time.time()
+    for t in range(nt):
+        NonLinCudaJit(u, dx, dt, un)
+    t2 = time.time()
+
+    print "Numbapro Cuda version took: %.6f seconds" % (t2-t1)
+    plt.plot(numpy.linspace(0,xmax,nx),u[:],marker='o',lw=2)
 
 
+#    t1 = time.time()
+#    u = NonLinVanilla(ui, nx, nt, dx, dt)
+#    t2 = time.time()
+#    print "Vanilla version took: %.6f seconds" % (t2-t1)
+#    
+#    plt.plot(numpy.linspace(0,xmax,nx),u[:],marker='o',lw=2)
+    plt.show()
+
+
+if __name__ == "__main__":
+        main()

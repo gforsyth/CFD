f5defa472818cd360c854b7e7b46fd32e26b6a4a
Gilbert Forsyth <gforsyth@hpcl-25.bu.edu>
2013-04-29 17:44:50 -0400

    will almost certainly fail, but better cuda syntax is in the making.  remember numbapro example on github for blackscholes

--- gpu_cavity.py
+++ gpu_cavity.py
@@ -3,8 +3,8 @@ from numbapro import autojit, cuda, jit, float32
 
 
 
-@jit(argtypes=[float32[:,:], float32[:,:], float32[:,:], float32, float32, float32, float32, float32, float32[:,:]], target='gpu')
-def CudaU(U, V, P, dx, dy, dt, rho, nu, UN):
+@jit(argtypes=[float32[:,:], float32[:,:], float32[:,:], float32, float32, float32, float32, float32, float32[:,:], float32[:,:]], target='gpu')
+def CudaU(U, V, P, dx, dy, dt, rho, nu, UN, VN):
     tidx = cuda.threadIdx.x
     blkidx = cuda.blockIdx.x
     blkdimx = cuda.blockDim.x
@@ -17,6 +17,9 @@ def CudaU(U, V, P, dx, dy, dt, rho, nu, UN):
 
     i = tidx + blkidx * blkdimx
     j = tidy + blkidy * blkdimy
+
+    if i >= U.shape[0] or j >= U.shape[1]:
+        return                              ###if you try to index out of the array bounds, kill the thread
 
 
     UN[i,j]=U[i,j]-U[i,j]*dt/dx*(U[i,j]-U[i-1,j])-\
@@ -34,22 +37,6 @@ def CudaU(U, V, P, dx, dy, dt, rho, nu, UN):
     elif j == height-1:
         UN[i, j] = 1
 
-
-@jit(argtypes=[float32[:,:], float32[:,:], float32[:,:], float32, float32, float32, float32, float32, float32[:,:]], target='gpu')
-def CudaV(U, V, P, dx, dy, dt, rho, nu, VN):
-    tidx = cuda.threadIdx.x
-    blkidx = cuda.blockIdx.x
-    blkdimx = cuda.blockDim.x
-
-    tidy = cuda.threadIdx.y
-    blkidy = cuda.blockIdx.y
-    blkdimy = cuda.blockDim.y
-
-    height, width = U.shape
-
-    i = tidx + blkidx * blkdimx
-    j = tidy + blkidy * blkdimy
-
     VN[i,j]=V[i,j]-U[i,j]*dt/dx*(V[i,j]-V[i-1,j])-\
         V[i,j]*dt/dy*(V[i,j]-V[i,j-1])-\
             dt/(2*rho*dx)*(P[i,j+1]-P[i,j-1])+\
@@ -65,6 +52,40 @@ def CudaV(U, V, P, dx, dy, dt, rho, nu, VN):
     elif j == height-1:
         VN[i, j] = 0
 
+    U[i,j] = UN[i,j]
+    V[i,j] = VN[i,j]
+
+
+#@jit(argtypes=[float32[:,:], float32[:,:], float32[:,:], float32, float32, float32, float32, float32, float32[:,:]], target='gpu')
+#def CudaV(U, V, P, dx, dy, dt, rho, nu, VN):
+#    tidx = cuda.threadIdx.x
+#    blkidx = cuda.blockIdx.x
+#    blkdimx = cuda.blockDim.x
+#
+#    tidy = cuda.threadIdx.y
+#    blkidy = cuda.blockIdx.y
+#    blkdimy = cuda.blockDim.y
+#
+#    height, width = U.shape
+#
+#    i = tidx + blkidx * blkdimx
+#    j = tidy + blkidy * blkdimy
+#
+#    VN[i,j]=V[i,j]-U[i,j]*dt/dx*(V[i,j]-V[i-1,j])-\
+#        V[i,j]*dt/dy*(V[i,j]-V[i,j-1])-\
+#            dt/(2*rho*dx)*(P[i,j+1]-P[i,j-1])+\
+#        nu*(dt/dx**2*(V[i+1,j]-2*V[i,j]+V[i-1,j])+\
+#            dt/dy**2*(V[i,j+1]-2*V[i,j]+V[i,j-1]))
+#
+#    if i == 0:
+#        VN[i, j] = 0
+#    elif i == width-1:
+#        VN[i, j] = 0
+#    elif j == 0:
+#        VN[i, j] = 0
+#    elif j == height-1:
+#        VN[i, j] = 0
+#
 @autojit
 def ppe(rho, dt, dx, dy, U, V, P):
     height, width = U.shape
@@ -112,24 +133,34 @@ def main():
     V = numpy.zeros((ny,nx), dtype=numpy.float32)
     P = numpy.zeros((ny, nx), dtype=numpy.float32)
 
-    UN = numpy.empty_like(U)
-    VN = numpy.empty_like(V)
+    UN = numpy.zeros((ny,nx), dtype=numpy.float32)
+    VN = numpy.zeros((ny,nx), dtype=numpy.float32)
+
+    
+    P = ppe(rho, dt, dx, dy, U, V, P)
+
 
     griddim = 1, 1
-    blockdim = nx, ny, 1
+    blockdim = 64, 64, 1
+    
+    stream = cuda.stream()
+    d_U = cuda.to_device(U, stream)
+    d_V = cuda.to_device(V, stream)
+    d_UN = cuda.to_device(UN, stream)
+    d_VN = cuda.to_device(VN, stream)
+   # d_P = cuda.to_device(P, stream)
 
-    CudaV_conf = CudaV[griddim, blockdim]
-    CudaU_conf = CudaU[griddim, blockdim]
+
+#    CudaV_conf = CudaV[griddim, blockdim]
+#    CudaU_conf = CudaU[griddim, blockdim]
 
 
     for n in range(nt):
+        CudaU[griddim, blockdim, stream](d_U, d_V, P, dx, dy, dt, rho, nu, d_UN, d_VN)
+        d_U.to_host(stream)
+        d_V.to_host(stream)
+        stream.synchronize()
         P = ppe(rho, dt, dx, dy, U, V, P)
-        CudaU_conf(U, V, P, dx, dy, dt, rho, nu, UN)
-        CudaV_conf(U, V, P, dx, dy, dt, rho, nu, VN)
-
-        U[:] = UN[:]
-        V[:] = VN[:]
-
 
 
 if __name__ == "__main__":

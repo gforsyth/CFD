3c3a60276022a2fd8da59d4ce2f3825d822d280e
Gil <gilforsyth@gmail.com>
2013-05-01 17:01:22 -0400

    attempt to place timeloop on gpu

--- gpu_cavity.py
+++ gpu_cavity.py
@@ -9,8 +9,8 @@ import math
 import time
 
 
-@jit(argtypes=[float32[:,:], float32[:,:], float32[:,:], float32, float32, float32, float32, float32, float32[:,:], float32[:,:]], target='gpu')
-def CudaU(U, V, P, dx, dy, dt, rho, nu, UN, VN):
+@jit(argtypes=[float32[:,:], float32[:,:], float32[:,:], float32, float32, float32, float32, float32, float32], target='gpu')
+def CudaU(U, V, P, dx, dy, dt, rho, nu, nt):
     tidx = cuda.threadIdx.x
     blkidx = cuda.blockIdx.x
     blkdimx = cuda.blockDim.x
@@ -21,76 +21,59 @@ def CudaU(U, V, P, dx, dy, dt, rho, nu, UN, VN):
 
     m, n = U.shape
 
+    UN = cuda.shared.array(shape=(m, n), dtype=float32)
+    VN = cuda.shared.array(shape=(m, n), dtype=float32)
+    
     i = tidx + blkidx * blkdimx
     j = tidy + blkidy * blkdimy
 
     if i >= U.shape[0] or j >= U.shape[1]:
         return                              ###if you try to index out of the array bounds, kill the thread
 
-    UN[i,j]=U[i,j]-U[i,j]*dt/dx*(U[i,j]-U[i-1,j])-\
-        V[i,j]*dt/dy*(U[i,j]-U[i,j-1])-\
-            dt/(2*rho*dx)*(P[i+1,j]-P[i-1,j])+\
-        nu*(dt/dx**2*(U[i+1,j]-2*U[i,j]+U[i-1,j])+\
-            dt/dy**2*(U[i,j+1]-2*U[i,j]+U[i,j-1]))
+    for n in range(nt):
+        P = ppe(rho, dt, dx, dy, U, V, P)
 
-    if i == 0:
-        UN[i, j] = 0
-    elif i == m-1:
-        UN[i, j] = 0
-    elif j == 0:
-        UN[i, j] = 0
-    elif j == n-1:
-        UN[i, j] = 1
+        UN[i,j]=U[i,j]-U[i,j]*dt/dx*(U[i,j]-U[i-1,j])-\
+            V[i,j]*dt/dy*(U[i,j]-U[i,j-1])-\
+                dt/(2*rho*dx)*(P[i+1,j]-P[i-1,j])+\
+            nu*(dt/dx**2*(U[i+1,j]-2*U[i,j]+U[i-1,j])+\
+                dt/dy**2*(U[i,j+1]-2*U[i,j]+U[i,j-1]))
 
-    VN[i,j]=V[i,j]-U[i,j]*dt/dx*(V[i,j]-V[i-1,j])-\
-        V[i,j]*dt/dy*(V[i,j]-V[i,j-1])-\
-            dt/(2*rho*dx)*(P[i,j+1]-P[i,j-1])+\
-        nu*(dt/dx**2*(V[i+1,j]-2*V[i,j]+V[i-1,j])+\
-            dt/dy**2*(V[i,j+1]-2*V[i,j]+V[i,j-1]))
+        if i == 0:
+            UN[i, j] = 0
+        elif i == m-1:
+            UN[i, j] = 0
+        elif j == 0:
+            UN[i, j] = 0
+        elif j == n-1:
+            UN[i, j] = 1
 
-    if i == 0:
-        VN[i, j] = 0
-    elif i == m-1:
-        VN[i, j] = 0
-    elif j == 0:
-        VN[i, j] = 0
-    elif j == n-1:
-        VN[i, j] = 0
-
-    U[i,j] = UN[i,j]
-    V[i,j] = VN[i,j]
+        cuda.syncthreads()
 
 
-#@jit(argtypes=[float32[:,:], float32[:,:], float32[:,:], float32, float32, float32, float32, float32, float32[:,:]], target='gpu')
-#def CudaV(U, V, P, dx, dy, dt, rho, nu, VN):
-#    tidx = cuda.threadIdx.x
-#    blkidx = cuda.blockIdx.x
-#    blkdimx = cuda.blockDim.x
-#
-#    tidy = cuda.threadIdx.y
-#    blkidy = cuda.blockIdx.y
-#    blkdimy = cuda.blockDim.y
-#
-#    height, width = U.shape
-#
-#    i = tidx + blkidx * blkdimx
-#    j = tidy + blkidy * blkdimy
-#
-#    VN[i,j]=V[i,j]-U[i,j]*dt/dx*(V[i,j]-V[i-1,j])-\
-#        V[i,j]*dt/dy*(V[i,j]-V[i,j-1])-\
-#            dt/(2*rho*dx)*(P[i,j+1]-P[i,j-1])+\
-#        nu*(dt/dx**2*(V[i+1,j]-2*V[i,j]+V[i-1,j])+\
-#            dt/dy**2*(V[i,j+1]-2*V[i,j]+V[i,j-1]))
-#
-#    if i == 0:
-#        VN[i, j] = 0
-#    elif i == width-1:
-#        VN[i, j] = 0
-#    elif j == 0:
-#        VN[i, j] = 0
-#    elif j == height-1:
-#        VN[i, j] = 0
-#
+        VN[i,j]=V[i,j]-U[i,j]*dt/dx*(V[i,j]-V[i-1,j])-\
+            V[i,j]*dt/dy*(V[i,j]-V[i,j-1])-\
+                dt/(2*rho*dx)*(P[i,j+1]-P[i,j-1])+\
+            nu*(dt/dx**2*(V[i+1,j]-2*V[i,j]+V[i-1,j])+\
+                dt/dy**2*(V[i,j+1]-2*V[i,j]+V[i,j-1]))
+
+        if i == 0:
+            VN[i, j] = 0
+        elif i == m-1:
+            VN[i, j] = 0
+        elif j == 0:
+            VN[i, j] = 0
+        elif j == n-1:
+            VN[i, j] = 0
+
+
+        U[i,j] = UN[i,j]
+        V[i,j] = VN[i,j]
+
+
+        cuda.syncthreads()
+
+
 @autojit
 def ppe(rho, dt, dx, dy, U, V, P):
     height, width = U.shape
@@ -143,13 +126,6 @@ def main():
     V = numpy.zeros((nx,ny), dtype=numpy.float32)
     P = numpy.zeros((ny, nx), dtype=numpy.float32)
 
-    UN = numpy.zeros((nx,ny), dtype=numpy.float32)
-    VN = numpy.zeros((nx,ny), dtype=numpy.float32)
-
-    
-    P = ppe(rho, dt, dx, dy, U, V, P)
-
-
     griddim = nx, ny
     blockdim = 768, 1, 1
     if nx > 767:
@@ -159,17 +135,12 @@ def main():
     stream = cuda.stream()
     d_U = cuda.to_device(U, stream)
     d_V = cuda.to_device(V, stream)
-    d_UN = cuda.to_device(UN, stream)
-    d_VN = cuda.to_device(VN, stream)
 
 
-    for n in range(nt):
-        P = ppe(rho, dt, dx, dy, U, V, P)
-        CudaU[griddim, blockdim, stream](d_U, d_V, P, dx, dy, dt, rho, nu, d_UN, d_VN)
-        d_U.to_host(stream)
-        d_V.to_host(stream)
-        stream.synchronize()
-        #P = ppe(rho, dt, dx, dy, U, V, P)
+    CudaU[griddim, blockdim, stream](d_U, d_V, P, dx, dy, dt, rho, nu, nt)
+    d_U.to_host(stream)
+    d_V.to_host(stream)
+    stream.synchronize()
 
     t2 = time.time()
 
@@ -177,9 +148,6 @@ def main():
     x = numpy.linspace(0,2,nx)
     y = numpy.linspace(0,2,ny)
     Y,X = numpy.meshgrid(y,x)
-
-    #import pdb
-    #pdb.set_trace()
     
     prescon = plt.figure()
     #plt.contourf(X[::10,::10],Y[::10,::10],P[::10,::10],alpha=0.5)
